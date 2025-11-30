d41f0805acfeedb03cfaaed7df2e5356b74960d5
Khai Vu <khaivuhuy0209@gmail.com>
2025-11-30 09:22:40 +0700

    gazebo rviz wordl

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,7 +2,7 @@ import os #Lấy đường dẫn thư mục package, Xử lý đường dẫn fi
 
 from ament_index_python.packages import get_package_share_directory # Lấy đường dẫn đến thư mục của package
 
-from launch import LaunchDescription #khung mô tả toàn bộ các hành động muốn chạy
+from launch import LaunchDescription #mô tả toàn bộ các hành động muốn chạy
 from launch.actions import IncludeLaunchDescription #include một file launch khác
 
 from launch.launch_description_sources import PythonLaunchDescriptionSource #cho phép include nhiều loại file launch
@@ -12,18 +12,20 @@ from launch_ros.actions import Node # Run Node
 def generate_launch_description():
     package_name = "articubot_one" # Khai bao package
 
+    # Tro den file rsp.launch.py
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    # Khoi dong gazebo 
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')])
     )
 
-    # Spawner node from the gazebo_ros package . The entity 
+    # Spawner node from the gazebo_ros package . The entity Include Robot vao trong Gazebo 
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
